78a139601b98bf26470546c8f774b612073f2d21
Mark Liffiton <liffiton@gmail.com>
2013-12-31 21:28:28 -0600

    Simplify data conversion.

--- LibTempTMP421.cpp
+++ LibTempTMP421.cpp
@@ -64,42 +64,19 @@
  * @return float - The local temperature in degrees C
  **********************************************************/
 float LibTempTMP421::GetTemperature(void) {
-    uint8_t in[2];
-    float frac = 0.0;
-    uint8_t bit;
+    int8_t high;  // signed: high byte is returned as signed 2's complement
+    uint8_t low;
 
-    setPtrLoc(0x00);                //high-byte
-    in[0] = getRegisterValue();
-    in[0] = getRegisterValue();
+    setPtrLoc(0x00);                //high-byte (integer part)
+    high = getRegisterValue();
+    high = getRegisterValue();
 
-    setPtrLoc(0x10);                //low-byte
-    in[1] = getRegisterValue();
-    in[1] = getRegisterValue();
-    in[1] >>=4;                     //shift-off the unused bits
-	
-    /* Assemble the fraction */
-    bit = in[1] & 0x01;
-    frac += (bit * 0.5) * (bit * 0.5) * (bit * 0.5) * (bit * 0.5);
+    setPtrLoc(0x10);                //low-byte (fractional part)
+    low = getRegisterValue();
+    low = getRegisterValue();
+    low >>=4;                       //shift-off the unused bits
 
-    in[1] >>= 1;
-    bit = in[1] & 0x01;
-    frac += (bit * 0.5) * (bit * 0.5) * (bit * 0.5);
-
-    in[1] >>= 1;
-    bit = in[1] & 0x01;
-    frac += (bit * 0.5) * (bit * 0.5);
-
-    in[1] >>= 1;
-    bit = in[1] & 0x01;
-    frac += (bit * 0.5);
-
-    /* Add the MSB to the fraction */
-    frac += in[0];
-
-	/* frac is unsigned, make it signed to allow for negative temps */
-	if (frac > 128.0)
-		frac -= 256;
-    return frac;
+    return high + (low / 16.0);
 }
 
 /**********************************************************
